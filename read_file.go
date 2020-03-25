fe876e20c95554aae93cc37415582ca29d26bb13
sehandev <sehandev@gmail.com>
2020-03-25 05:21:43 +0000

    ADD : read_file.go comment

--- read_file.go
+++ read_file.go
@@ -9,6 +9,7 @@ import (
 )
 
 func checkOpenFile(err error) {
+	// file이 올바르게 열렸는지 확인
 	if err != nil {
 		log.Fatalf("open file error: %v", err)
 		panic(err)
@@ -16,6 +17,7 @@ func checkOpenFile(err error) {
 }
 
 func checkScanFile(err error) {
+	// EOF까지 올바르게 scan했는지 확인
 	if err != nil {
 		log.Fatalf("scan file error: %v", err)
 		panic(err)
@@ -24,16 +26,17 @@ func checkScanFile(err error) {
 
 func readFile(fileName string) {
 	// beginTime := time.Now()
-	// fileName := "data/data.txt"
 
 	fp, err := os.OpenFile(fileName, os.O_RDONLY, os.ModePerm)
 	checkOpenFile(err)
 	defer fp.Close()
 
 	scanner := bufio.NewScanner(fp)
-	for scanner.Scan() {
-		line := scanner.Text()
+	for scanner.Scan() { // 한 줄 스캔, EOF -> 반복 종료
+		line := scanner.Text() // 스캔한 문장을 string으로 변환
+
 		if strings.Contains(line, "사랑") {
+			// 문장에 단어 "사랑"이 포함되어 있으면
 			fmt.Println(line)
 		}
 	}
